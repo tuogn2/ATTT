2d8ed25ffd0e631788ba7cb72321d18344efc116
Tony Do (FED) <107382964+Tony-Do-FED@users.noreply.github.com>
2023-11-18 19:58:04 +0700

    delete url encode

--- src/component/Encryption.js
+++ src/component/Encryption.js
@@ -5,31 +5,32 @@ import sha1 from 'sha1'
 import crc32 from 'crc32'
 import encodeUrl from 'encodeurl'
 function Encryption() {
+    const saltRounds = 10;
     const options = [
         { value: 'MD5' },
         { value: 'SHA-1' },
-        { value: 'EncodeURL' },
         { value: 'CRC32' }
-       
+
     ]
+   
     const [typeOfEncode, setTypeOfEncode] = useState('')
-    const [text,setText] = useState('')
-    const [affterEncode,setAffterEncode] = useState('')
-    const handleEncode =()=>{
-        if(typeOfEncode ==='MD5'){
-            
-            const k =md5(text)
+    const [text, setText] = useState('')
+    const [affterEncode, setAffterEncode] = useState('')
+    const handleEncode = () => {
+        if (typeOfEncode === 'MD5') {
+
+            const k = md5(text)
             setAffterEncode(k)
-        }else if(typeOfEncode ==='SHA-1'){
-            
-            const k =sha1(text)
+        } else if (typeOfEncode === 'SHA-1') {
+
+            const k = sha1(text)
             setAffterEncode(k)
-        }else if(typeOfEncode ==='CRC32'){
-            
-            const k =crc32(text)
+        } else if (typeOfEncode === 'CRC32') {
+            const k = crc32(text)
             setAffterEncode(k)
         }
-        else if(typeOfEncode ==='EncodeURL'){
+        else if (typeOfEncode === 'EncodeURL') {
+
             const k =encodeUrl(text)
             setAffterEncode(k)
         }
@@ -48,7 +49,7 @@ function Encryption() {
             <div className="text-xl mb-3 mt-3">
                 <b>Nhập ký tự</b>
             </div>
-            <Input placeholder="Mời nhập ký tự" value={text}  onChange={(event)=>setText(event.target.value)}/>
+            <Input placeholder="Mời nhập ký tự" value={text} onChange={(event) => setText(event.target.value)} />
             <Button className='m-5' onClick={handleEncode} >Mã hóa</Button>
             <div>
                 {affterEncode}
